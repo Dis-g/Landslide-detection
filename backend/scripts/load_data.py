8448b8e65d30be8e9c7bb1698178a761192dcfed
kanishkgits <kanishkcodes@gmail.com>
2025-11-07 01:00:56 +0530

    loading in the tif data files in python

---
+++ backend/scripts/load_data.py
@@ -0,0 +1,12 @@
+import rasterio
+import numpy as np
+
+with rasterio.open('../dataset/Darjeeling_Elevation.tif') as src:
+    elevation = src.read(1)
+
+print(elevation.shape)
+
+with rasterio.open('../dataset/Darjeeling_Slope.tif') as src:
+    slope = src.read(1)
+
+print(slope.shape)
